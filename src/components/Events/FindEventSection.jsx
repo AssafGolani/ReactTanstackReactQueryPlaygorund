47a1055ce24f97be8769c9937cabce4b42499367
Assaf <1214assaf@gmail.com>
2024-06-06 16:42:29 +0300

    refactor: Enabled & disabled queries.

--- src/components/Events/FindEventSection.jsx
+++ src/components/Events/FindEventSection.jsx
@@ -6,10 +6,11 @@ import ErrorBlock from "../UI/ErrorBlock.jsx";
 import EventItem from "./EventItem.jsx";
 export default function FindEventSection() {
   const searchElement = useRef();
-  const [searchTerm, setSearchTerm] = useState("");
-  const { data, isPending, isError, error } = useQuery({
+  const [searchTerm, setSearchTerm] = useState();
+  const { data, isLoading, isError, error } = useQuery({
     queryKey: ["events", { search: searchTerm }],
     queryFn: ({ signal }) => fetchEvents({ signal, searchTerm }),
+    enabled: searchTerm !== undefined,
   });
   function handleSubmit(event) {
     event.preventDefault();
@@ -18,7 +19,7 @@ export default function FindEventSection() {
 
   let content = <p>Please enter a search term and to find events.</p>;
 
-  if (isPending) {
+  if (isLoading) {
     content = <LoadingIndicator />;
   }
 
