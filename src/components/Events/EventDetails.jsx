626b22896492d30d8037dfdfabb80bcc50e036d4
Assaf <1214assaf@gmail.com>
2024-06-08 09:45:53 +0300

    feat: Add delete functionality to EventDetails component
    The code changes in `EventDetails.jsx` add the ability to delete an event. This is achieved by using the `useMutation` hook from `@tanstack/react-query` to handle the deletion mutation. The `handleDelete` function triggers the mutation, which calls the `deleteEvent` function from `http.js` and updates the `queryClient` to invalidate the "events" query. After a successful deletion, the component navigates to the "/events" page. This new functionality improves the user experience by allowing events to be easily deleted.

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,17 +1,30 @@
-import { Link, Outlet } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
+import { Link, Outlet, useNavigate } from "react-router-dom";
+import { useQuery, useMutation } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import Header from "../Header.jsx";
-import { fetchEvent } from "../../util/http.js";
+import { fetchEvent, deleteEvent, queryClient } from "../../util/http.js";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EventDetails() {
+  const navigate = useNavigate();
   const { id } = useParams();
   const { data, isPending, isError, error } = useQuery({
     queryKey: ["events", id],
     queryFn: ({ signal }) => fetchEvent({ id, signal }),
   });
   const { title, location, date, time, description, image } = data ?? {};
+
+  const { mutate } = useMutation({
+    mutationFn: deleteEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["events"] });
+      navigate("/events");
+    },
+  });
+
+  function handleDelete() {
+    mutate({ id });
+  }
 
   let content;
   if (isPending) {
@@ -47,7 +60,7 @@ export default function EventDetails() {
         <header>
           <h1>{title}</h1>
           <nav>
-            <button>Delete</button>
+            <button onClick={handleDelete}>Delete</button>
             <Link to="edit">Edit</Link>
           </nav>
         </header>
